a77f46779bfa370f8325d4c78fd918cf68c1c1a0
RajeshRaman63 <rajeshraman63@gmail.com>
2021-03-28 01:24:45 +0530

    f

--- test5.cpp
+++ test5.cpp
@@ -46,4 +46,55 @@ int main() {
         
     }
 
+<<<<<<< Updated upstream
+=======
+    if(root->right){
+        retRight = hasPathSum(root->right,sum-root->value,vect,ans);
+    }
+
+    /*if(retLeft || retRight){
+        ans.push_back(vect);
+        vect.clear();
+    }*/
+
+    return retLeft || retRight;
+}
+
+int main(){
+
+
+    Node* root=new Node(5);
+    root->left= new Node(4);
+    root->right = new Node(8);
+    root->left->left=new Node(11);
+    root->left->left->left=new Node(7);
+    root->left->left->right=new Node(2);
+    root->right->left=new Node(13);
+    root->right->right=new Node(4);
+    root->right->right->right=new Node(1);
+
+
+//    zigzag(root);
+
+
+    int sum = 22;
+
+	vector<vector<int>> ans;
+
+    vector<int> vect;
+
+    cout<<hasPathSum(root,sum,vect,ans)<<endl;
+
+	for(int i=0;i<ans.size();i++){
+		for(int j =0;j<ans[i].size();j++){
+			cout<<ans[i][j]<<"  ";
+		}
+		cout<<endl;
+	}
+
+    cout<<"I am writting this in test branch for testing purpose. "<<endl;
+
+    return 0;
+
+>>>>>>> Stashed changes
 }
