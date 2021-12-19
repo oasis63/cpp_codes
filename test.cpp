3a3b0c4de79fc1c6e9e23d0ff0776571197ca5de
RajeshRaman63 <rajeshraman63@gmail.com>
2021-12-19 15:20:37 +0530

    test.cpp changes

--- test.cpp
+++ test.cpp
@@ -1,68 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define fast ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
-#define ll long long 
+void subset(int num[],int n){
 
-class Solution{
-    public:
-        void arrange(vector<int> &A);
-};
+	int p = 1<<n;  // pow(2,n) .. no. of subsets
 
-void printVector(vector<int> vect){
-    int n = vect.size();
-    for(int i=0;i<n;i++){
-        cout<<vect[i]<<" ";
-    }
-    cout<<endl;
-}
+	for(int i=0;i<p;i++){
 
-void Solution::arrange(vector<int> &A) {
-   int n = A.size();
-
-    // using extra space 
-   vector<int> tempVect(n);
-   printVector(tempVect);
-
-   for(int i=0;i<n;i++){
-       tempVect[A[i]]=i;
-   }    
-
-    cout<<"Answer vector"<<endl;
-    printVector(tempVect);
+		for(int j=0;j<n;j++){
+			if((i&(1<<j)))
+				cout<<num[j]<<" ";
+		}
+		cout<<endl;
+	}
 
 }
 
 int main(){
 
-    Solution s;
+    // 3
+    // 1 1 2
 
-    // vector<int> vect{1,0};
-    vector<int> vect{0,2,4,1,3};   // 0 4 3 2 1  
-    // vector<int> vect{ 4, 0, 2, 1, 3};
+    int n;
+    cin>>n;
 
+    int arr[n];
 
-    cout<<vect.size()<<endl;
+    subset(num,3);
 
-    string str = "fsdlfjasjf";
-
-
-    cout<<"string size : ";
-    cout<<str.length()<<endl;
-
-
-
-    s.arrange(vect);
-
-
-    
-
-
-    
-
-    vect.size();
-
-    // printVector(vect);    
-
-    return 0;
 }
