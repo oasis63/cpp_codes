466bbde9a37a2588cd77301f5d69451593bfa62b
rajeshbosak <rajeshbosak89@gmail.com>
2025-08-04 05:07:52 +0530

    🧨 temp: YOLO commit

--- test1.cpp
+++ test1.cpp
@@ -3,48 +3,17 @@
 #include "UTILS/helper.h"
 using namespace std;
 
-#define fast ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
-#define ll long long
-
-// 	Largest Product in a Series
-
-ll products(string str) {
-  ll prod = 1;
-  int n = str.length();
-  for (int i = 0; i < n; i++) {
-    if (str[i] == '0')
-      return 0;
-    int d = str[i] - '0';
-    prod *= d;
-  }
-  return prod;
-}
-
 int main() {
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
+  set_io_files("input.txt", "output.txt");
 
-  string line;
-  getline(cin, line);
+  int n = 0;
 
-  string str{};
-
-  int n = line.size();
-
-  for (int i = 0; i < 13; i++) {
-    str.push_back(line[i]);
+  for (int i = 0; i < 3; i++) {
+    n = n | (1 << i);
+    bug(n);
   }
 
-  ll prod = products(str);
-
-  for (int i = 13; i < n; i++) {
-    str.erase(0, 1);
-    str.push_back(line[i]);
-
-    prod = max(prod, products(str));
-  }
-
-  bug(prod);
+  bug(n);
 
   return 0;
 }
