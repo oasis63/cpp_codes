198bfd4746a3a6fae5e9495ef56c46e9731904d9
rajeshbosak <rajeshbosak89@gmail.com>
2025-08-04 01:35:05 +0530

    🧠 chore: pretending I understand pointers

--- test.cpp
+++ test.cpp
@@ -1,38 +1,10 @@
 #include <bits/stdc++.h>
 
 #include "UTILS/helper.h"
-
 using namespace std;
 
-class Solution {
- public:
-  int solve(vector<int> &vect) {
-    int ans = 0;
-
-    return ans;
-  }
-};
-
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
-
   set_io_files("input.txt", "output.txt");
-
-  Solution sol;
-
-  string line;
-  getline(cin, line);
-
-  vector<int> nums = parseVector<int>(line);
-
-  printVect(nums);
-
-  cout << "Solution started ...." << endl;
-  int ans = sol.solve(nums);
-
-  cout << "ans : " << ans << endl;
 
   return 0;
 }
