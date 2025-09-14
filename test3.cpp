026ea1c1e16ff42abed4e1483b8464d227014c74
rajeshbosak <rajeshbosak89@gmail.com>
2025-09-15 01:30:15 +0530

    🌳 feat: shook the family tree

--- test3.cpp
+++ test3.cpp
@@ -1,81 +1,24 @@
 #include <bits/stdc++.h>
 
 #include "UTILS/helper.h"
-
 using namespace std;
 
-class Solution {
- public:
-  int evalRPN(vector<string>& tokens) {
-    stack<int> stk;
-
-    string operands = "+-*/";
-
-    for (auto& tok : tokens) {
-      // cout << tok << endl;
-
-      if (operands.find(tok) != string::npos) {
-        // cout << "token : " << endl;
-        // let's say the stack always has size more than 2
-
-        int num1 = stk.top();
-        stk.pop();
-        int num2 = stk.top();
-        stk.pop();
-
-        int res = 0;
-
-        if (tok == "+") {
-          res = num1 + num2;
-        } else if (tok == "-") {
-          res = num2 - num1;
-        } else if (tok == "*") {
-          res = num1 * num2;
-        } else {
-          res = num2 / num1;
-        }
-
-        stk.push(res);
-
-      } else {
-        int num = stoi(tok);
-        // cout << "number " << stoi(tok) << endl;
-        stk.push(num);
-      }
-    }
-
-    int result = stk.top();
-
-    // bug(result);
-
-    // bug(stk.size());
-
-    // return 0;
-
-    return result;
-  }
-};
-
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+  string b = "cdabcdab";
+  string a = "abcdabcdabcd";  // "abcd";  // 3
 
-  set_io_files("input.txt", "output.txt");
+  int ind = a.find(b);
 
-  Solution sol;
+  bug(ind);
 
-  string line;
-  getline(cin, line);
+  string str1 = "ab";
+  string str2 = "cdaadab";
 
-  vector<string> tokens = parseVector<string>(line);
+  int i = str2.find(str1);
 
-  printVect(tokens);
+  bug(i);
 
-  cout << "Solution started ...." << endl;
-  int ans = sol.evalRPN(tokens);
-
-  cout << "ans : " << ans << endl;
+  bug(string::npos);
 
   return 0;
 }
